bacc2106870a68ec73253a09a0e3427010df5cdc
kaderny <kaderbrklyn@outlook.com>
2020-11-05 12:10:29 -0500

    fixed bug for submit button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 
+function App() {
+
+const [name , setName]= useState(""); 
+const [name2 , setName2]= useState(""); 
+
 function handleClick(){
-  [name , setName]= useState("") ;
- 
-   
+  setName2(name); 
 }
 
 
 function handleChange(event){
-  console.log(event.target.value);
+ // console.log(event.target.value);
   setName(event.target.value);
 }
 
-function App() {
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input onChange= {handleChange} type="text" placeholder="What's your name?" />
-      <button type = "Submit" onClick= {handleClick}>Submit</button>
+     <h1>Hello {name2}</h1> 
+     
+      <input onChange= {handleChange} type="text" placeholder="What's your name?"  value= {name}/>
+      <button  onClick= {handleClick}>Submit</button>
     </div>
   );
 }
